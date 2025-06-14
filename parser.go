56c90bfa53de2535071b8738c347a15c0d09d542
epicCoolDog <epiccooldog@techie.com>
2025-06-14 15:54:24 -0400

    Tokenizer

--- parser.go
+++ parser.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func parser(tokens [][]string) error {
+	for _, line := range tokens {
+		if line[0] == "print" {
+			if len(line) < 2 {
+				return fmt.Errorf("error: missing argument for print statement")
+			}
+
+			if strings.HasPrefix(line[1], "\"") && strings.HasSuffix(line[1], "\"") {
+				fmt.Println(line[1][1 : len(line[1])-1])
+			} else if val, err := strconv.Atoi(line[1]); err == nil {
+				fmt.Println(val)
+			} else if val, err := strconv.ParseFloat(line[1], 64); err == nil {
+				fmt.Println(val)
+			}
+		}
+	}
+
 	return nil
 }
--- tokenizer.go
+++ tokenizer.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"slices"
 	"strings"
 )
+
+var operators = []string{"+", "-", "*", "/", "%", "^", ">", "<", "=", "[", "]", "{", "}", "(", ")", ","}
 
 // line is a struct that represents a line of code
 type line struct {
@@ -11,8 +14,78 @@ type line struct {
 
 // Tokenize splits a line of code into tokens
 func (l line) Tokenize() ([]string, error) {
+	l.text = strings.TrimRight(l.text, " \t\r")
+
+	// Line content variable to be appended to
 	line_content := []string{}
-	line_content = append(line_content, l.text)
+
+	// Variables to track the state of the tokenizer
+	start_line := true
+	var string_creating bool
+
+	// Current token for tokens larger than 1 character
+	var current_token string
+
+	for _, char := range l.text {
+		// Whitespace for indentation
+		if char == ' ' && start_line {
+			line_content = append(line_content, " ")
+
+			continue
+		} else {
+			start_line = false
+		}
+
+		// Checks if a string is being created
+		if char == '"' {
+			string_creating = !string_creating
+			current_token += "\""
+
+			if !string_creating {
+				line_content = append(line_content, current_token)
+				current_token = ""
+			}
+			continue
+		}
+
+		// Checks if a string is being created
+		if string_creating {
+			current_token += string(char)
+			continue
+		}
+
+		// Checks if the current character is an operator
+		if slices.Contains(operators, string(char)) {
+			if current_token != "" {
+				line_content = append(line_content, current_token)
+			}
+
+			line_content = append(line_content, string(char))
+			current_token = ""
+
+			continue
+		}
+
+		// Checks if the current character is a space
+		if char == ' ' {
+			if current_token != "" {
+				line_content = append(line_content, current_token)
+			}
+			current_token = ""
+
+			continue
+		}
+
+		current_token += string(char)
+	}
+
+	// Appends the last token
+	if current_token != "" {
+		line_content = append(line_content, current_token)
+	}
+
+	//fmt.Println(strings.Join(line_content, " | "))
+
 	return line_content, nil
 }
 
